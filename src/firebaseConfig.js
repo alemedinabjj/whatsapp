50484e24bba98c87cc89c3af4d3c7fd3adbec494
alemedinabjj <101877712+alemedinabjj@users.noreply.github.com>
2022-09-05 16:31:23 -0300

    revert

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 
 export default {
-  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID,
+  apiKey: 'AIzaSyA6cxRCj9_jQJnN_7TGz2AV1KKdOVQC5vc',
+  authDomain: "whatsappclone-42556.firebaseapp.com",
+  projectId: "whatsappclone-42556",
+  storageBucket: "whatsappclone-42556.appspot.com",
+  messagingSenderId: "27640341004",
+  appId: "1:27640341004:web:e26050a521cc62d276c429"
 }
